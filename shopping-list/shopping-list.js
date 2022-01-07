ef259d3e415611b7bd4f56a2664112f13d578aa2
Sam-Melius <91688565+Sam-Melius@users.noreply.github.com>
2022-01-06 17:01:58 -0700

    create

--- shopping-list/shopping-list.js
+++ shopping-list/shopping-list.js
@@ -2,8 +2,24 @@ import { checkAuth, logout } from '../fetch-utils.js';
 
 checkAuth();
 
+const form = document.querySelector('.item-form');
+const deleteButton = document.querySelector('.delete'),
+const listEl = document.querySelector('.list')
+
 const logoutButton = document.getElementById('logout');
 
 logoutButton.addEventListener('click', () => {
     logout();
+});
+
+form.addEventListener('submit', async(e) => {
+    e.preventDefault();
+
+    const data = FormData(form);
+    const item = data.get('item');
+    const quantity = data.get('quantity');
+
+    await createListItem(item, quantity);
+
+    form.reset();
 });
